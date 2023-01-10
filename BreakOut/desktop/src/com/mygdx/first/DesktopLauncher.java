5aaf097e02aa1efad9ce1c40d7c6218443e0ccf5
Zack Leya <zleya96@gmail.com>
2023-01-10 13:14:46 -0500

    Startup issue fixed

--- BreakOut/desktop/src/com/mygdx/first/DesktopLauncher.java
+++ BreakOut/desktop/src/com/mygdx/first/DesktopLauncher.java
@@ -10,6 +10,8 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("first-game");
+
 		new Lwjgl3Application(new FirstGame(), config);
+
 	}
 }
